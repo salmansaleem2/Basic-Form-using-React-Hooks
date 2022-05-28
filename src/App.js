8084665967c8793791480cf837d46295c1db6308
salmansaleem2 <salmansaleem8523@gmail.com>
2022-05-28 20:49:26 +0500

    basic form using hooks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 export default function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const [valid, setValid] = useState(false);
+
+  const handleFirstNameInputChange = (event) => {
+    setValues({ ...values, firstName: event.target.value });
+  };
+  const handleLastNameInputChange = (event) => {
+    setValues({ ...values, lastName: event.target.value });
+  };
+  const handleEmailInputChange = (event) => {
+    setValues({ ...values, email: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (values.firstName && values.lastName && values.email) {
+      setValid(true);
+    }
+    setSubmitted(true);
+  };
+
   return (
     <div class="form-container">
-      <form class="register-form">
+      <form class="register-form" onClick={handleSubmit}>
         {/* Uncomment the next line to show the success message */}
-        {/* <div class="success-message">Success! Thank you for registering</div> */}
+        {submitted && valid ? (
+          <div class="success-message">Success! Thank you for registering</div>
+        ) : null}
         <input
           id="first-name"
+          value={values.firstName}
+          onChange={handleFirstNameInputChange}
           class="form-field"
           type="text"
           placeholder="First Name"
           name="firstName"
         />
         {/* Uncomment the next line to show the error message */}
-        {/* <span id="first-name-error">Please enter a first name</span> */}
+        {submitted && !values.firstName ? (
+          <span id="first-name-error">Please enter a first name</span>
+        ) : null}
         <input
           id="last-name"
+          value={values.lastName}
+          onChange={handleLastNameInputChange}
           class="form-field"
           type="text"
           placeholder="Last Name"
           name="lastName"
         />
         {/* Uncomment the next line to show the error message */}
-        {/* <span id="last-name-error">Please enter a last name</span> */}
+        {submitted && !values.lastName ? (
+          <span id="last-name-error">Please enter a last name</span>
+        ) : null}
+
         <input
           id="email"
+          value={values.email}
+          onChange={handleEmailInputChange}
           class="form-field"
           type="text"
           placeholder="Email"
           name="email"
         />
         {/* Uncomment the next line to show the error message */}
-        {/* <span id="email-error">Please enter an email address</span> */}
+        {submitted && !values.email ? (
+          <span id="email-error">Please enter an email address</span>
+        ) : null}
+
         <button class="form-field" type="submit">
           Register
         </button>
